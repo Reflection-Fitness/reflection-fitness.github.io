410b122ed32e83cda87a2fe932cfaba33269257d
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2021-01-02 20:52:16 -0500

    setup

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello User!</h1>
+      <p>
+        Sorry! This website is currently under construction.
+        Please check back shortly. Until then, please contact info@reflectionfitness.net for any inquiries.
+        Thank you for your patience.
+      </p>
+    </div>
+  );
+}
+
+export default App;
