c260db459bfd28deda9b9bff0d71882469c68b93
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2021-01-16 18:10:12 -0500

    rework text sizes + about page

--- app/src/pages/AboutPage/AboutPage.js
+++ app/src/pages/AboutPage/AboutPage.js
@@ -1,8 +1,30 @@
-import { Navbar } from '../../components/Navbar/Navbar';
 import './AboutPage.scss';
+import { Navbar } from '../../components/Navbar/Navbar';
+import { Footer } from '../../components/Footer/Footer';
+import bg from '../../assets/backgrounds/About.png';
+import headshot from '../../assets/Headshot.svg';
+
+import mountW from '../../assets/graphics/MountainsW.svg';
+import mountT from '../../assets/graphics/MountainsT.svg';
 
 export function AboutPage(){
     return (
-        <Navbar curr="About"/>
+        <>
+            <Navbar curr="About"/>
+            <div className="about container">
+                <img src={bg} alt="background" className="bg" />
+                <div className="hero">
+                    <img src={headshot} alt="headshot"/>
+                    <div className="detail">
+                        <h1>Victoria Samuel</h1>
+                        <p>Our proud ownder with 6 years of experience in the fitness industry</p>
+                    </div>
+                </div>
+
+                <img src={mountW} alt="mountain" className="mount w"/>
+                <img src={mountT} alt="mountain" className="mount t"/>
+            </div>
+            <Footer curr="About"/>
+        </>
     );
 }
