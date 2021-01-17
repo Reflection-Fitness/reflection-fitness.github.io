b461f8ae38e5ca20091be33f100428d221a7b295
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2021-01-17 18:52:02 -0500

    form UI and input binding - TODO: add emailjs

--- app/src/components/ActionButton/ActionButton.js
+++ app/src/components/ActionButton/ActionButton.js
@@ -2,9 +2,9 @@ import './ActionButton.scss';
 import arrow from '../../assets/graphics/GreenArrow.svg';
 import { Link } from 'react-router-dom';
 
-export function ActionButton({txt="action-btn", link="/contact"}){
+export function ActionButton({txt="action-btn", link="", onClick=null, props}){
     return (
-        <Link to={link} className="action-btn">
+        <Link to={link} className="action-btn" onClick={onClick} {...props}>
             <p>{txt}</p>
             <img src={arrow} alt="arrow"/>
         </Link>
--- app/src/pages/ContactPage/ContactPage.js
+++ app/src/pages/ContactPage/ContactPage.js
@@ -1,13 +1,90 @@
 import { Navbar } from '../../components/Navbar/Navbar';
 import { Footer } from '../../components/Footer/Footer';
+import { ActionButton } from '../../components/ActionButton/ActionButton';
 import './ContactPage.scss';
 import { Socials } from '../../components/Socials/Socials';
+import bg from '../../assets/backgrounds/Contact.png';
+import { useRef, useState } from 'react';
+
+function OpenHour({ day, time }) {
+    return (
+        <div className="hour">
+            <p>{day}</p>
+            <p>{time}</p>
+        </div>
+    );
+}
+
+function useFormFields(initialValues) {
+    const [formFields, setFormFields] = useState(initialValues);
+    const createChangeHandler = (key) => (
+        e
+    ) => {
+        const value = e.target.value;
+        setFormFields((prev) => ({ ...prev, [key]: value }));
+    };
+    return { formFields, createChangeHandler, setFormFields };
+}
+
+function FormField({ name, dispTxt, inputProps, fieldProps }) {
+    return (
+        <div className="field" {...fieldProps}>
+            <input
+                id={name}
+                type="text"
+                autoComplete="off"
+                required
+                className="input"
+                {...inputProps}
+            />
+            <label htmlFor={name}>
+                <span className="content-name">{dispTxt}</span>
+            </label>
+        </div>
+    );
+}
 
 export function ContactPage() {
+    const formEl = useRef(null);
+
+    const { formFields, createChangeHandler, setFormFields } = useFormFields({
+        name: "",
+        email: "",
+        phone: "",
+        message: ""
+    })
+
+    const [status, setStatus] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        let emailRe = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let phoneRe = /^[0-9\b]+$/;
+        if (formFields.name.length <= 0) {
+            setStatus("Invalid Full Name");
+        } else if (!emailRe.test(formFields.email)) {
+            setStatus("Invalid Email Address");
+        } else if (!phoneRe.test(formFields.phone)) {
+            setStatus("Invalid Phone Number");
+        } else if (formFields.message.length <= 0) {
+            setStatus("Invalid Message");
+        } else {
+            setFormFields({
+                name: "",
+                email: "",
+                phone: "",
+                message: ""
+            })
+            setStatus("Submitted!");
+        }
+    }
+
     return (
         <>
             <Navbar curr="Contact" />
             <div className="contact container">
+                <img src={bg} alt="background" className="bg" />
                 <div className="hero">
                     <h1>Get In Touch</h1>
                     <p>
@@ -18,8 +95,75 @@ export function ContactPage() {
                     <Socials />
                 </div>
 
-                <div className="contact">
-                    
+                <div className="card">
+                    <div className="send-msg">
+                        <h1>Send us a Message</h1>
+                        <form ref={formEl}>
+                            <div className="section">
+                                <FormField
+                                    name="name"
+                                    dispTxt="Full Name"
+                                    inputProps={{
+                                        value: formFields.name,
+                                        onChange: createChangeHandler("name")
+                                    }}
+                                />
+                                <FormField
+                                    name="email"
+                                    dispTxt="Email"
+                                    inputProps={{
+                                        value: formFields.email,
+                                        onChange: createChangeHandler("email")
+                                    }}
+                                />
+                                <FormField
+                                    name="phone"
+                                    dispTxt="Phone"
+                                    inputProps={{
+                                        value: formFields.phone,
+                                        onChange: createChangeHandler("phone")
+                                    }}
+                                />
+                            </div>
+                            <div className="spacer" />
+                            <div className="section">
+                                <div className="field multi">
+                                    <textarea
+                                        rows="7"
+                                        cols="33"
+                                        autoComplete="off"
+                                        required
+                                        className="input"
+                                        value={formFields.message}
+                                        onChange={createChangeHandler("message")}
+                                    />
+                                    <label>
+                                        <span className="content-name">Message</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <ActionButton
+                                txt="Send"
+                                link=""
+                                onClick={onSubmit}
+                                props={{
+                                    type: "submit",
+                                    className: "action-btn submit"
+                                }}
+                            />
+                        </form>
+                        <p className="status">{status}</p>
+                    </div>
+
+                    <div className="hours">
+                        <h1>Hours</h1>
+                        <OpenHour day="Monday" time="7 AM - 6 PM" />
+                        <OpenHour day="Tuesday" time="7 AM - 6 PM" />
+                        <OpenHour day="Wednesday" time="7 AM - 6 PM" />
+                        <OpenHour day="Thursday" time="7 AM - 6 PM" />
+                        <OpenHour day="Friday" time="7 AM - 6 PM" />
+                        <OpenHour day="Saturday" time="8 AM - 1 PM" />
+                    </div>
                 </div>
             </div>
             <Footer curr="Contact" />
